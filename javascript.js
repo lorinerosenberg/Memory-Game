8f3e3fa51c90bed8c8f76e9e9097529cd5f7df5c
lorinerosenberg <lennylourosenberg@gmail.com>
2017-07-04 09:29:20 +0300

    1.01
    Basic layout done dynamically with for loop

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
 /**
  * Created by hillarosenberg on 2017/07/03.
  */
+
+
+//4*5 card setup
+for(var i = 0; i<4; i++){
+    var rows = document.createElement('div');
+    document.body.appendChild(rows);
+    rows.id = "cardsRow"
+
+    for(var r = 0; r<5; r++){
+        var cards = document.createElement('div');
+        rows.appendChild(cards);
+        cards.id = "card";
+    }
+}
+
+
+
+
+
