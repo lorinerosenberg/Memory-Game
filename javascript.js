ef804fd24d7a3863b22d85fa0cdd394e9b3850cd
lorinerosenberg <lennylourosenberg@gmail.com>
2017-07-05 16:38:52 +0300

    1.06
    shuffle cards working - need to add new game and high score

--- javascript.js
+++ javascript.js
@@ -2,9 +2,6 @@
  * Created by hillarosenberg on 2017/07/03.
  */
 
-// create random shuffle of arrays to assign as divs
-var idArray = [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10];
-idArray.sort(function(a, b){return 0.5 - Math.random()});
 
 //stores global variables
 var cardID = document.getElementsByClassName("card");
@@ -15,6 +12,10 @@ var correctGuesses = 0;
 
 //function to randomly assign ids to cards "shuffle"
 function shuffleCards() {
+    // create random shuffle of arrays to assign as divs
+    var idArray = [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10];
+    idArray.sort(function(a, b){return 0.5 - Math.random()});
+    //loop to assign id's to cards
         for (var i = 0; i < idArray.length; i++) {
             cardID[i].setAttribute("data-id", idArray[i]);
             cardID[i].style.backgroundImage = "url(images/" + cardID[i].getAttribute("data-id") + ".jpg)";
@@ -46,11 +47,16 @@ function flipCard() {
 
 //function to see if chosen cards match and to keep count of matches
 function checkCards(){
+    //once counter reaches 10 all cards have been matched
     if(correctGuesses===9){
         console.log("you won!");
     }
     else if(firstCard.getAttribute("data-id") === secondCard.getAttribute("data-id")){
+        //add to counter for each correct guess
         correctGuesses += 1;
+        //remove event listener after listener after cards have been clicked
+        firstCard.removeEventListener("click",flipCard);
+        secondCard.removeEventListener("click",flipCard);
     }
     else{
         firstCard.style.opacity = "0";
@@ -58,5 +64,19 @@ function checkCards(){
     }
     choices = 0;
 }
+
+//function to start new game
+function newGame(){
+    choices = 0;
+    var card = document.getElementsByClassName("cards");
+    card.style.opacity = "0";
+    shuffleCards();
+}
+
+
+//event listener to shuffle cards after page load
+document.getElementById("shuffle").addEventListener("click", shuffleCards);
+//event listener to start new game
+document.getElementById("new").addEventListener("click", newGame);
 
 
